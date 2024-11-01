383ace2d5bc5c601e987a794de6c053ae8c356f1
0mori1 <117737361+0mori1@users.noreply.github.com>
2024-11-01 17:41:41 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,4 +5,6 @@ do
   git pull
   chmod u+x ./launch.sh
   ~/zulu8.70.0.23-ca-jdk8.0.372-linux_x64/bin/java -XX:+UseG1GC -Xmx7G -Xms7G -Dsun.rmi.dgc.server.gcInterval=600000 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32 -jar server.jar nogui
+  echo "Restarting in 5 seconds! CTRL + C to stop."
+  sleep 5
 done
